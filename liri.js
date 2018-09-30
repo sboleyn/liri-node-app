7774b0280a2e2f10dbce375db8680992b91a8e78
Sami B <sboleyn@email.arizona.edu>
2018-09-29 22:23:13 -0700

    start feature 2

--- liri.js
+++ liri.js
@@ -12,34 +12,24 @@ var argms = process.argv;
 var action = argms[2];
 var value = argms.splice(3).join(' ');
 
-// spotify
-//   .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-//   .then(function (data) {
-//     console.log(data);
-//   })
-//   .catch(function (err) {
-//     console.error('Error occurred: ' + err);
-//   });
-
-
 // ---------------------------
 // FUNCTION 1
 // ---------------------------
 
 // `node liri.js concert-this <artist/band name here>`
-if (action === "concert-this"){
+if (action === "concert-this") {
   var url = "https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp";
-  request(url, function(err, resp, body){
-    if (!err && resp.statusCode === 200){
+  request(url, function (err, resp, body) {
+    if (!err && resp.statusCode === 200) {
       var _body = JSON.parse(body);
 
-      console.log("The artist/band "+ value + " has these upcoming events! (" + _body.length +")");
+      console.log("The artist/band " + value + " has these upcoming events! (" + _body.length + ")");
 
       var bandCount = 0;
-      _body.forEach(function(band){
+      _body.forEach(function (band) {
         bandCount += 1;
 
-        var location = band.venue.city + ", " +band.venue.region +' '+ band.venue.country;
+        var location = band.venue.city + ", " + band.venue.region + ' ' + band.venue.country;
 
         // var _date = moment(band.dateTime, 'YYYY-MM-DDHH:MM:SS').format('MM/DD/YYYY');
 
@@ -51,17 +41,40 @@ if (action === "concert-this"){
         console.log("Venue Name: " + band.venue.name);
         console.log("Venue Location: " + location);
         // console.log("Event Date: " + _date);
-        console.log("Event Date: "+moment(band.datetime).format('MM/DD/YYYY'));
+        console.log("Event Date: " + moment(band.datetime).format('MM/DD/YYYY'));
         console.log('*-----------------------------------*\n');
       })
     }
   })
 }
 
-// "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-//      * Name of the venue
-// * Venue location
-// * Date of the Event (use moment to format this as "MM/DD/YYYY")
+// ---------------------------
+// FUNCTION 2
+// ---------------------------
 
+if (action === "spotify-this-song") {
+  // value is a song
+  // search: function({ type: 'artist OR album OR track', query: 'My search query', limit: 20 }, callback);
 
+  // spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  //   if (err) {
+  //     return console.log('Error occurred: ' + err);
+  //   }
 
+  // console.log(data); 
+  // });
+
+  spotify.search({ type: 'track', query: value, limit: 20 }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    else {
+      console.log("You've searched for the track: " + value);
+      console.log(data.tracks.items[0]);
+      // data.tracks.items.forEach(function(song){
+   
+         //   console.log(song);
+      // })
+    }
+  })
+}
